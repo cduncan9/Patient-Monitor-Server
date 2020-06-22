57a2d02c0f696c466b621df9cad80eb584df8e23
Canyon Duncan <canyon.duncan@duke.edu>
2020-06-22 11:43:02 -0400

    created the design_window() function and set up root

--- monitoring_station_gui_client.py
+++ monitoring_station_gui_client.py
@@ -1,3 +1,13 @@
 import tkinter as tk
 from tkinter import ttk
 
+
+def design_window():
+    root = tk.Tk()
+    root.title("Monitoring Station User Interface")
+
+    root.mainloop()
+
+
+if __name__ == '__main__':
+    design_window()
