c2f0ff51b3899076b0aec48457db6830ea70fbd4
Canyon Duncan <canyon.duncan@duke.edu>
2020-06-22 11:49:37 -0400

    Added a label for the patient ID and a ComboBox without anything in it

--- monitoring_station_gui_client.py
+++ monitoring_station_gui_client.py
@@ -6,6 +6,12 @@ def design_window():
     root = tk.Tk()
     root.title("Monitoring Station User Interface")
 
+    patient_id_text = ttk.Label(root, text="Select Patient ID")
+    patient_id_text.grid(column=0, row=0)
+
+    patient_id_box = ttk.Combobox(root)
+    patient_id_box.grid(column=0, row=1)
+
     root.mainloop()
 
 
