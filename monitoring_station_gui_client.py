f90b4aea5c09e1f23291ae09a89d80b2c820f8f1
Canyon Duncan <canyon.duncan@duke.edu>
2020-06-22 13:02:05 -0400

    Created a button that calls a function to load patient data

--- monitoring_station_gui_client.py
+++ monitoring_station_gui_client.py
@@ -8,6 +8,10 @@ def get_available_patient_ids():
     return "001"  # This is just a place holder
 
 
+def load_patient_data():
+    return
+
+
 def design_window():
     root = tk.Tk()
     root.title("Monitoring Station User Interface")
@@ -17,8 +21,12 @@ def design_window():
 
     file_choice = tk.StringVar()
     patient_id_box = ttk.Combobox(root, textvariable=file_choice)
-    patient_id_box['values']
+    patient_id_box['values'] = get_available_patient_ids()
     patient_id_box.grid(column=0, row=1)
+
+    load_patient_button = ttk.Button(root, text="Load Patient Data",
+                                     command=load_patient_data)
+    load_patient_button.grid(column=1, row=1)
 
     root.mainloop()
 
