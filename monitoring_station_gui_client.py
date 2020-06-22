9084a4bbbb3d0d7e43cb790a9da6ed68ee9d515b
Canyon Duncan <canyon.duncan@duke.edu>
2020-06-22 12:06:40 -0400

    Created a function to get the patient ids, returns a placeholder id currently, does not yet make a request

--- monitoring_station_gui_client.py
+++ monitoring_station_gui_client.py
@@ -2,14 +2,22 @@ import tkinter as tk
 from tkinter import ttk
 
 
+def get_available_patient_ids():
+    # This should make a request to the server and return
+    # available patient_ids
+    return "001"  # This is just a place holder
+
+
 def design_window():
     root = tk.Tk()
     root.title("Monitoring Station User Interface")
 
     patient_id_text = ttk.Label(root, text="Select Patient ID")
-    patient_id_text.grid(column=0, row=0)
+    patient_id_text.grid(column=0, row=0, sticky="W")
 
-    patient_id_box = ttk.Combobox(root)
+    file_choice = tk.StringVar()
+    patient_id_box = ttk.Combobox(root, textvariable=file_choice)
+    patient_id_box['values']
     patient_id_box.grid(column=0, row=1)
 
     root.mainloop()
