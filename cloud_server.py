73059a8920b2c4da9e9266d9ab56c7fcd227c409
Canyon Duncan <canyon.duncan@duke.edu>
2020-06-23 11:29:34 -0400

    Created the class NewPatient which is used to store patient info

--- cloud_server.py
+++ cloud_server.py
@@ -2,9 +2,19 @@ from flask import Flask, jsonify, request
 from datetime import datetime
 import requests
 import logging
+from pymodm import connect, MongoModel, fields
 
 
 app = Flask(__name__)
+
+
+class NewPatient(MongoModel):
+    patient_id = fields.IntegerField()
+    patient_name = fields.CharField()
+    heart_rate = fields.ListField()
+    timestamp = fields.ListField()
+    ecg_images = fields.ListField()
+    medical_images = fields.ListField()
 
 
 if __name__ == '__main__':
