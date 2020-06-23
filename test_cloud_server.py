a433ae5e0b10cb110b4a6a7f9892a643fc24998b
aidan-therien <aidan.therien@duke.edu>
2020-06-23 15:22:27 -0400

    trying to get pytest to work

--- test_cloud_server.py
+++ test_cloud_server.py
@@ -1,6 +1,9 @@
 import pytest
 from pymodm import connect, MongoModel, fields
+from cloud_server import NewPatient
+from setup_test_db import init_test_db
 
+init_test_db()
 
 
 @pytest.mark.parametrize('patient_id, expected',
@@ -13,5 +16,11 @@ def test_check_patient_exists(patient_id, expected):
     assert answer == expected
 
 
-if __name__ == '__main__':
-    init_test_db()
+@pytest.mark.parametrize('patient_id, expected',
+                         [("1000", True),
+                          ("2000", True),
+                          ("1234", False)])
+def test_check_patient_exists(patient_id, expected):
+    from cloud_server import check_patient_exists
+    answer = check_patient_exists(patient_id)
+    assert answer == expected
